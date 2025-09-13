231e162f132c6b262c5d028dd66adc12b4647d69
a2112 <aduty@students.kennesaw.edu>
2025-09-12 22:44:00 -0400

    code update

--- convert.c
+++ convert.c
@@ -1,28 +1,74 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 
+char convertDigit(unsigned d) {
+   if (d < 10) {
+      return (char)('0' + d);
+   } else {
+      return (char)('A' + (d-10));
+   }
+}
+
+// Division Algorithm
 void div_convert(uint32_t n, int base, char *out) {
-   char temp[65]; // Temporary buffer
+   char temp[65];
    int pos = 0;
 
-   // Handle zero case
    if (n == 0) {
       strcpy(out, "0");
       return;
    }
 
-   // Extract digits from right to left
    while (n > 0) {
       int remainder = n % base;
       n = n / base;
 
-      // Convert digit to character
       if (remainder < 10)
          temp[pos++] = '0' + remainder;
       else
          temp[pos++] = 'A' + (remainder- 10);
    }
 
-   // Reverse the result
-   // TODO: Your code here!
+   for (int i = 0; i < pos; i++) {
+        out[i] = temp[pos - i - 1];
+    }
+    out[pos] = '\0';
 }
+
+// Subtraction Algorithm
+void sub_convert(uint32_t n, int base, char* out) {
+
+   
+   int pos = 0, startFlag = 0;
+   uint32_t power = 1;
+
+    if (n == 0) {
+        out[pos++] = '0';
+        out[pos] = '\0';
+        return;
+    }
+
+    while (power <= n / (uint32_t)base) {
+      power *= (uint32_t)base;
+    }
+
+    while (power > 0) {
+      unsigned digit = (unsigned)(n / power);
+      if (digit != 0 || startFlag) {
+         out[pos++] = convertDigit(digit);
+         startFlag = 1;
+      }
+      
+      n -= (uint32_t)digit * power;
+      power /= (uint32_t)base;
+    }
+   
+    if (!startFlag) {
+      out[pos++] = '0';
+    }
+
+    out[pos] = '\0';
+   }
